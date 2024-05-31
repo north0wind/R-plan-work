19dfaefd308a3b8a36c63e9527d34b222a904b87
hb15305610801 <lixl77@chinatelecom.cn>
2024-05-31 17:00:25 +0800

    修改comments-service的问题

--- comments-service/src/main/java/com/comment/springboot/core/CommentsServiceApplication.java
+++ comments-service/src/main/java/com/comment/springboot/core/CommentsServiceApplication.java
@@ -1,5 +1,6 @@
 package com.comment.springboot.core;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -7,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author xiaolong
  * @date 2024/5/31
  */
+@MapperScan("com.comment.springboot.core.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CommentsServiceApplication {
